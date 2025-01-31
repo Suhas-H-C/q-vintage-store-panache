c7c412258585f1eafb735c767c46f2dba599b9c3
Suhas H C <suhashc193@gmail.com>
2025-01-31 16:06:05 +0530

    tests added

--- vintage-store/src/test/java/org/shc/quarkus/panache/resources/ArtistResourceTest.java
+++ vintage-store/src/test/java/org/shc/quarkus/panache/resources/ArtistResourceTest.java
@@ -1,10 +1,13 @@
 package org.shc.quarkus.panache.resources;
 
+import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 class ArtistResourceTest {
@@ -22,11 +25,43 @@ class ArtistResourceTest {
     }
 
     @Test
+    void shouldReturnArtistById() {
+        given()
+                .when()
+                .get("/v1/artist/1")
+                .then()
+                .log()
+                .all()
+                .statusCode(204);
+    }
+
+    @Test
     void shouldThrowBadRequestWhenUnknownContextIsPassed() {
         given()
                 .when()
                 .get("/v1/unknown")
                 .then()
                 .statusCode(404);
+    }
+
+    @Test
+    @TestTransaction
+    void shouldPersistAnArtistAndReturnTheSame() {
+        String requestBody = """
+                    {
+                        "bio": "Persisting the developer as an artist",
+                        "name": "Suhas H C"
+                    }
+                """;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/v1/artist")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Suhas H C"))
+                .body("bio", equalTo("Persisting the developer as an artist"));
     }
 }
